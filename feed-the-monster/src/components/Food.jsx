0f087ead1a369b8d9c1e666c3ca275e71ccd9dab
Aaron Weidele <aweidele@gmail.com>
2025-04-11 09:55:39 -0400

    Make food draggable

--- feed-the-monster/src/components/Food.jsx
+++ feed-the-monster/src/components/Food.jsx
@@ -1,11 +1,21 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { draggable } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 import invariant from "tiny-invariant";
 
 export const Food = ({ src, alt, isFood }) => {
+  const ref = useRef(null);
+  const [dragging, setDragging] = useState(false);
+
+  useEffect(() => {
+    const el = ref.current;
+    invariant(el);
+
+    return draggable({ element: el, onDragStart: () => setDragging(true), onDrop: () => setDragging(false) });
+  }, []);
+
   return (
-    <div>
-      <img src={src} alt={alt} />
+    <div ref={ref} className={dragging ? "opacity-50" : null}>
+      <img src={src} alt={alt} draggable="false" />
     </div>
   );
 };
